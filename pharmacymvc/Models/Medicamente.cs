720e4483613ddc0a887162c246488b60e28b3004
zloisupport <kr0t77@yandex.ru>
2021-02-17 22:00:41 +0600

    Add project files.

---
+++ pharmacymvc/Models/Medicamente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pharmacymvc.Models
+{
+    public class Medicamente
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+        public string name { get; set; }
+
+    }
+}
